0f6facb42d3296d07f230853a644734e6ec61600
Diego Ordóñez <diego.ordonez@gmail.com>
2021-07-22 15:25:26 -0500

    Initial

---
+++ src/main/java/dordonez/clients/fotos_rest_cli/RestClientFotosApplication.java
@@ -0,0 +1,13 @@
+package dordonez.clients.fotos_rest_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientFotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientFotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dordonez/clients/fotos_rest_cli/RestClientFotosApplicationTests.java
@@ -0,0 +1,13 @@
+package dordonez.clients.fotos_rest_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientFotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
